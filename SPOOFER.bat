5c4a8fdaf62a2094e9f23e52edd2e22a38586bcd
Mfzaum <105993637+Mfzaum@users.noreply.github.com>
2023-06-27 13:54:40 -0700

    Update SPOOFER.bat

--- SPOOFER.bat
+++ SPOOFER.bat
@@ -1,9 +1,1 @@
-AMIDEWINx64.exe /su
-AMIDEWINx64.exe /bs SPACEX-%random%-%random%-Spoofer
-AMIDEWINx64.exe /ss SPACEX-%random%-%random%-Spoofer
-AMIDEWINx64.exe /id SPACEX-%random%-Spoofer
-AMIDEWINx64.exe /iv SPACEX-%random%-Spoofer
-AMIDEWINx64.exe /ivn SPACEX-%random%-Spoofer
-AMIDEWINx64.exe /sm SPACEX-%random%-Spoofer
-AMIDEWINx64.exe /sp SPACEX-%random%-Spoofer
-exit
+ipconfig
