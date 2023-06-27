a67a8e6a05a0e39c22919e71ee3afd77de1daac8
Mfzaum <105993637+Mfzaum@users.noreply.github.com>
2023-06-27 13:46:13 -0700

    Add files via upload

---
+++ SPOOFER.bat
@@ -0,0 +1,9 @@
+AMIDEWINx64.exe /su
+AMIDEWINx64.exe /bs SPACEX-%random%-%random%-Spoofer
+AMIDEWINx64.exe /ss SPACEX-%random%-%random%-Spoofer
+AMIDEWINx64.exe /id SPACEX-%random%-Spoofer
+AMIDEWINx64.exe /iv SPACEX-%random%-Spoofer
+AMIDEWINx64.exe /ivn SPACEX-%random%-Spoofer
+AMIDEWINx64.exe /sm SPACEX-%random%-Spoofer
+AMIDEWINx64.exe /sp SPACEX-%random%-Spoofer
+exit
